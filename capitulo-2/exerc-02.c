28e3a08346fabec609096e3c9c895726a3c6c8c5
Thiago <thiagocarvalhal77@gmail.com>
2023-11-26 21:59:49 -0300

    Entrada e saída

---
+++ capitulo-2/exerc-02.c
@@ -0,0 +1,12 @@
+/*
+Escreva um programa que leia um número inteiro e depois o imprima
+*/
+
+#include <stdio.h>
+
+int main(){
+    int num;
+    scanf("%d", &num);
+    printf("%d\n", num);
+    return 0;
+}
