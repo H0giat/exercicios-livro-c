96acded4dd7ac014866fcb8e80015878ba91e628
Thiago <thiagocarvalhal77@gmail.com>
2023-11-26 21:50:39 -0300

    Primeiro exercício

---
+++ capitulo-2/exerc-01.c
@@ -0,0 +1,11 @@
+/*
+Elabore um programa que escreva as mensagens "Início do programa" e "Fim" na tela,
+uma em cada linha, usando apenas o comando printf()
+*/
+
+#include <stdio.h>
+
+int main(){
+    printf("Inicio do programa\nFim\n");
+    return 0;
+}
