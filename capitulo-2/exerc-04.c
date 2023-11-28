dddfb7a898d223745cb76312201279ff1397c38b
Thiago <thiagocarvalhal77@gmail.com>
2023-11-28 12:29:27 -0300

    Retorna um erro com casas decimais

---
+++ capitulo-2/exerc-04.c
@@ -0,0 +1,13 @@
+/*
+Faça um programa que leia um número inteiro e depois o imprima usando o operador "%f".
+Veja o que aconteceu.
+*/
+
+#include <stdio.h>
+
+int main(){
+    int num;
+    scanf("%d", &num);
+    printf("%f\n", num);
+    return 0;
+}
