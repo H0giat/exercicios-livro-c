7e3122cf0365f4cd71d63d62cc0568e1ef61f789
Thiago <thiagocarvalhal77@gmail.com>
2023-11-28 12:20:42 -0300

    Lendo um inteiro

---
+++ capitulo-2/exerc-03.c
@@ -0,0 +1,12 @@
+/*
+Escreva um programa que leia um número inteiro e depois imprima a mensagem "Valor lido:",
+seguido do valor inteiro. Use apenas um comando printf()
+*/
+
+#include <stdio.h>
+
+int main(){
+    int num;
+    scanf("%d", &num);
+    printf("Valor lido: %d", num);
+}
