1f771b344f8d167e43cc59f21c7f113383a29de8
Thiago <thiagocarvalhal77@gmail.com>
2024-01-22 10:18:11 -0300

    Pequena correção

--- capitulo-2/exerc-03.c
+++ capitulo-2/exerc-03.c
@@ -8,5 +8,6 @@ seguido do valor inteiro. Use apenas um comando printf()
 int main(){
     int num;
     scanf("%d", &num);
-    printf("Valor lido: %d", num);
+    printf("Valor lido: %d\n", num);
+    return 0;
 }
