b96c1dcfae148e4b518116bf8116b51c0ab4a5e6
Thiago <thiagocarvalhal77@gmail.com>
2024-01-25 16:57:37 -0300

    Constante

---
+++ capitulo-2/exerc-11.c
@@ -0,0 +1,12 @@
+/*
+Elabore um programa que contenha uma constante qualquer do tipo float. Use o comando #define.
+Imprima essa constante.
+*/
+
+#include <stdio.h>
+#define PI 3.14
+
+int main(){
+    printf("%f\n", PI);
+    return 0;
+}
